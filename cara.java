a7ce3f05873979d86e8e43480dc65c1d2a412289
Kreggy <filip.makrlik@gmail.com>
2021-09-13 10:04:29 +0200

    hm

---
+++ cara.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    boolean err = true;
+        String input = "0";
+	    do {
+	        err = true;
+            System.out.println("Jak dlouhou čáru chceš vykreslit?");
+
+            input = sc.nextLine();
+            if(input.matches("\\d+")) err = false;
+            else System.out.println("Prosím napiš číslo");
+        }while(err);
+
+	    int val = Integer.parseInt(input);
+
+	    for(int i = 0; i < val; i++) {
+            System.out.print(".");
+        }
+
+    }
+}
